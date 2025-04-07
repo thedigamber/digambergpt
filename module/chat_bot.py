b802651baa2800aef4262de8d384a45a45ef8404
thedigamber <digamberraj7903@gmail.com>
2025-04-07 13:16:11 +0530

    Delete module directory

--- module/chat_bot.py
+++
@@ -1,9 +0,0 @@
-
-
-def chatbot_ui():
-    user_input = st.text_input('Ask me anything')
-    if user_input:
-        st.info('Typing...')
-        response = generate_gemini_response(user_input)
-        st.success(response)
-      

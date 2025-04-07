80382022dc9272853af12787a0daf825eebffeca
thedigamber <digamberraj7903@gmail.com>
2025-04-07 20:20:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -256,7 +256,7 @@ with tab1:
         text = text.lower()
         abuse_keywords = ["madarchod", "bhosdi", "chutiya", "gaand", "bhenchod", "loda", "fuck", "suck", "stupid", "idiot"]
         disrespect_keywords = ["tu kya", "tum kya", "bakwass", "chup", "gandu", "behen ke", "tatti", "chomu", "nalle", "jhatu"]
-        return any(word in text for word in abuse_keywords + disrespect keywords)
+        return any(word in text for word in abuse_keywords + disrespect_keywords)
 
     # --- On Submit ---
     if query and query.strip():
@@ -339,4 +339,4 @@ else:
         """<a href="https://drive.google.com/uc?export=download&id=1cdDIcHpQf-gwX9y9KciIu3tNHrhLpoOr" target="_blank">
         <button style='background-color:green;color:white;padding:10px 20px;border:none;border-radius:8px;font-size:16px;'>Download Android APK</button></a>""",
         unsafe_allow_html=True
-            )
+    )

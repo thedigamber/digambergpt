a8cbe988c0619a32c5e480f4feb5fb956b63b458
thedigamber <digamberraj7903@gmail.com>
2025-04-07 12:44:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 import time
 import random
 
-from Modules.chatbot import chatbot_ui
+from modules.chatbot import chatbot_ui
 from modules.image_gen import image_gen_ui
 from modules.file_reader import file_reader_ui
 from modules.settings import settings_ui

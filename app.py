85b5a5fcbdee80db05349c09caf6ad9264459e26
thedigamber <digamberraj7903@gmail.com>
2025-04-07 10:24:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -325,4 +325,4 @@ else:
         """<a href="https://drive.google.com/uc?export=download&id=1cdDIcHpQf-gwX9y9KciIu3tNHrhLpoOr" target="_blank">
         <button style='background-color:green;color:white;padding:10px 20px;border:none;border-radius:8px;font-size:16px;'>Download Android APK</button></a>""",
         unsafe_allow_html=True
-                )
+            )

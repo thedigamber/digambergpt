73b543964e40de0385c40a441a3ff988e0488592
thedigamber <digamberraj7903@gmail.com>
2025-04-07 13:12:27 +0530

    Create Apk_download.py

---
+++ modules/Apk_download.py
@@ -0,0 +1,8 @@
+
+
+def apk_ui():
+    st.markdown(
+        """<a href="https://drive.google.com/uc?export=download&id=1cdDIcHpQf-gwX9y9KciIu3tNHrhLpoOr" target="_blank">
+        <button style='background-color:green;color:white;padding:10px 20px;border:none;border-radius:8px;font-size:16px;'>Download Android APK</button></a>""",
+        unsafe_allow_html=True
+    )
